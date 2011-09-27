f19fc3d740039b8275089e73eecb4ca100ae3f02
Wiggins <wiggins@concentricsky.com>
2011-09-27 10:56:46 -0700

    initial import

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Populate',
+    version='0.5.0',
+    description='Python functions for generating random values, intended to use to randomize datasets',
+    author='Wiggins',
+    author_email='django@concentricsky.com',
+    url='http://github.com/coffindragger/populate',
+)
